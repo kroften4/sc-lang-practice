78186dc621261d39a0d38f30023799c6bc609bed
Stanislav <sskozhemyakin2006@gmail.com>
2026-03-11 22:57:52 +0300

    fix assets path

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,9 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			assets: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+            relative: true
 		}
 	},
 	// preprocess: vitePreprocess()
